7846fd691967b78d29c4ccb644b31f3ce7106193
bahriilhame <bahri.ilhame@gmail.com>
2023-12-17 01:47:43 +0100

    added the ressources to work with

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: true, // Set to true if you want to use CSS modules
+  },
+})
